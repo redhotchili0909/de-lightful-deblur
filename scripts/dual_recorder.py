dcd7f7e7d51132d15daabf8eb4097b0478bcce61
amandalchang <113650953+amandalchang@users.noreply.github.com>
2024-12-06 12:01:16 -0500

    adding duration argument

--- scripts/dual_recorder.py
+++ scripts/dual_recorder.py
@@ -5,27 +5,33 @@ from picamera2 import Picamera2, Preview
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FfmpegOutput
 
-CAM_A_RESOLUTION = (1920, 1080)
-CAM_B_RESOLUTION = (720, 480)
-
-CAM_A_FRAMERATE = 10
-CAM_B_FRAMERATE = 100
-
-VID_DURATION = 10 # in seconds
-
 # initialize argument parser
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", action='store_true', help = "Run in test mode (won't save videos)")
 parser.add_argument(
     "-f", 
     type=str, 
-    metavar="BASE_NAME", 
+    metavar="Video file base name", 
     default="camera", 
     help="Specify a base name for the video files (e.g., 'video' will save as 'video_a.mp4' and 'video_b.mp4')."
 )
-
+parser.add_argument(
+    "-d",
+    type=int,
+    metavar="Video Duration",
+    default=10,
+    help="Specify a video duration in seconds"
+)
 parser.parse_args()
 args = parser.parse_args()
+
+CAM_A_RESOLUTION = (1920, 1080)
+CAM_B_RESOLUTION = (720, 480)
+
+CAM_A_FRAMERATE = 10
+CAM_B_FRAMERATE = 100
+
+VID_DURATION = args.d # in seconds
 
 camera_a = Picamera2(0)
 config_a = camera_a.create_video_configuration(main={"size":CAM_A_RESOLUTION})
@@ -36,6 +42,7 @@ camera_b = Picamera2(1)
 config_b = camera_b.create_video_configuration(main={"size":CAM_B_RESOLUTION})
 camera_b.configure(config_b)
 camera_b.start_preview(Preview.QT, x=500,y=300,width=400,height=300)
+if args.t: print("This is just a test and I will not save a file")
 
 camera_a.set_controls({"FrameRate":CAM_A_FRAMERATE})
 
@@ -47,7 +54,6 @@ camera_b.start()
 time.sleep(2)
 
 if args.t:
-    print("This is just a test and I will not save a file")
     time.sleep(VID_DURATION)
 else:
     encodera = H264Encoder(1000000)
